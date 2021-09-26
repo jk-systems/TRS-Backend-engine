482eeb91444c8c385bc1f82cd1927fe032b4a407
jk-systems <jkmwadime@gmail.com>
2021-09-26 06:55:42 +0300

    First server stucture push --test

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require("path")
+const express = require("express")
+const dotenv = require('dotenv')
+const bodyparser = require('body-parser')
+
+const app = express()
+dotenv.config()
+
+const router = require("./src/router")
+const port = process.env.PORT
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static("public"))
+app.set("views", "views")
+app.set("view engine", "hbs")
+
+app.use("/", router)
+
+app.listen(port, () => {
+  console.log(`The server is now running on Port : ${port}`)
+})
